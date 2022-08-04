ae509f614e8191b4d7fc1220923034b653f510bd
Reza Asadolahi <rezaasadollahi0021@gmail.com>
2022-08-04 12:12:14 +0430

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const { graphqlHTTP } = require('express-graphql')
 require('dotenv').config({ path: './config.env' })
+const { graphqlHTTP } = require('express-graphql')
 const schema = require('./Services/GraphQL/schema')
 
 
